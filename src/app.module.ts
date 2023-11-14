23880e1335f65c80dc94ceba1537e6864e9ae37e
eiice <eicexxk@gmail.com>
2023-11-14 16:40:49 -0300

    Adding seprated PrismaService with a Prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
-
 import { UsersModule } from './modules/users/users.module';
+import { PrismaModule } from './modules/prisma/prisma.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { APP_GUARD, Reflector } from '@nestjs/core';
+import { JwtAuthGuard } from 'src/modules/auth/jwt-auth.guard';
 
 @Module({
-  imports: [UsersModule],
+  imports: [AuthModule, UsersModule, PrismaModule, ConfigModule.forRoot()],
+  providers: [
+    JwtService,
+    {
+      provide: APP_GUARD,
+      useFactory: (ref) => new JwtAuthGuard(ref),
+      inject: [Reflector],
+    },
+  ],
 })
 export class AppModule {}
