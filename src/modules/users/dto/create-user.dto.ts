336888e9758fc69fa4bb6b3c0da24e597bdb13e3
eiice <eicexxk@gmail.com>
2023-11-14 16:42:15 -0300

    Modifying dependencies

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsNotEmpty, IsEmail, MinLength, MaxLength, ValidateIf, IsEmpty } from 'class-validator';
-import { IsRequiredIfAdministrator } from 'src/modules/common/validators/isRequiredAdministrator';
+import { IsString, IsNotEmpty, IsEmail, MinLength, MaxLength, IsEmpty, Validate } from 'class-validator';
+import { IsRequiredWhenIsAdministrator } from 'src/common/validators/isRequiredWhenIsAdministrator';
 
 export class CreateUserDto {
     @ApiProperty()
@@ -29,9 +29,8 @@ export class CreateUserDto {
     type: string
 
     @ApiProperty()
-    @IsString()
-    @ValidateIf((o) => o.type === 'administrator', {
-      message: 'The area field is required when type is set to administrator.',
+    @Validate(IsRequiredWhenIsAdministrator, {
+        message: "The area field is required when 'type' is set to 'administrator'.",
     })
     area: string;
 
