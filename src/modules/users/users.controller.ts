fbe6db0cab49800a9e28007672cb17eb9833252d
eiice <eicexxk@gmail.com>
2023-11-14 16:39:21 -0300

    Prettier indentation

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,26 +1,40 @@
-import { Controller, Post, Body, Get, Put, Param, Delete, ValidationPipe } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Put,
+  Param,
+  Delete,
+  ValidationPipe,
+} from '@nestjs/common';
+import { ApiResponse } from '@nestjs/swagger';
 import { UsersService } from './users.service';
+import { UserEntity } from './entity/users.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { DeleteUserDto } from './dto/delete-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
-import { get } from 'http';
-import { ApiResponse } from '@nestjs/swagger';
+import { IsPublic } from '../auth/is-public.decorator';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post('signup')
-  @ApiResponse({type: CreateUserDto, isArray: true})
-  async create(@Body(new ValidationPipe({ transform: true })) data: CreateUserDto) {
+  @IsPublic()
+  @ApiResponse({ type: CreateUserDto, isArray: true })
+  async create(
+    @Body(new ValidationPipe({ transform: true })) data: CreateUserDto,
+  ) {
     return this.usersService.create(data);
   }
 
-  @Post('login')
-  async login(@Body() data: LoginUserDto) {
-    return this.usersService.login(data);
-  }
+//   @Post('login')
+//   @IsPublic()
+//   @ApiResponse({ type: UserEntity })
+//   async login(@Body() data: LoginUserDto) {
+//     return this.usersService.login(data);
+//   }
 
   @Get()
   async findAll() {
@@ -33,28 +47,28 @@ export class UsersController {
   }
 
   @Delete(':id')
-  async delete(@Param('id') id: string, @Body() data: DeleteUserDto) {
-    return this.usersService.delete(id, data);
+  async delete(@Param('id') id: string) {
+    return this.usersService.delete(id);
   }
 }
 
-
-@Controller('admin') 
+@Controller('admin')
 export class AdminController {
-    constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) {}
 
-    @Post('login')
-    async login(@Body() data: LoginUserDto) {
-      return this.usersService.loginAdmin(data);
-    }
+  @Post('login')
+  @IsPublic()
+  async login(@Body() data: LoginUserDto) {
+    return this.usersService.loginAdmin(data);
+  }
 
-    @Get('reports')
-    async reports() {
-        return this.usersService.adminGetReports();
-    }
+  @Get('reports')
+  async reports() {
+    return this.usersService.adminGetReports();
+  }
 
-    @Get('users')
-    async getUsers() {
-        return this.usersService.adminGetUsers();
-    }
+  @Get('users')
+  async getUsers() {
+    return this.usersService.adminGetUsers();
+  }
 }
